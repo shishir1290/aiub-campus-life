6fa2ce54bfc69173997fd10c299950abfe2113a9
Md. Sadmanur Islam Shishir <shishir1290@gmail.com>
2024-05-26 14:27:53 +0600

    send mail

--- src/pages/api/sendEmail.tsx
+++ src/pages/api/sendEmail.tsx
@@ -6,10 +6,10 @@ export default async (req: any, res: any) => {
 
     // Create a transporter object
     const transporter = nodemailer.createTransport({
-      service: 'gmail', 
+      service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER, 
-        pass: process.env.EMAIL_PASS, 
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
